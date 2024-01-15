4f00ef1dfa864632c712638d7e2432bcea3f6c6f
H4MZ403 <hamza.hmz2001@gmail.com>
2024-01-15 01:09:21 +0100

    add market-place-mobile-app

---
+++ marketplacefrontend/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "market-place-frontend"
+include(":app")
